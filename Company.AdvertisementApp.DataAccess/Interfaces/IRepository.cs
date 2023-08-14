32bed0be585eba06f041cd03dfc2a4974b6efa1d
Sinan Çoban <sinan.coban@outlook.com>
2023-08-14 17:05:44 +0300

    Dataccess and Repository added

---
+++ Company.AdvertisementApp.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,8 @@
+﻿using Company.AdvertisementApp.Entities;
+
+namespace Company.AdvertisementApp.DataAccess.Interfaces;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    
+}
---
+++ Company.AdvertisementApp.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq.Expressions;
+using Company.AdvertisementApp.DataAccess.Contexts;
+using Company.AdvertisementApp.DataAccess.Interfaces;
+using Company.AdvertisementApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.AdvertisementApp.DataAccess.Repositories;
+
+public class Repository<T> : IRepository<T> where T : BaseEntity
+{
+    private readonly AdvertisementContext _context;
+
+    public Repository(AdvertisementContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+    public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter)
+    {
+        return await _context.Set<T>().Where(filter).ToListAsync();
+    }
+    public async Task<List<T>> GetAllAsync<TKey>(Expression<Func<T, TKey>> selector)
+    {
+        return await _context.Set<T>().OrderBy(selector).ToListAsync();
+    }
+}
