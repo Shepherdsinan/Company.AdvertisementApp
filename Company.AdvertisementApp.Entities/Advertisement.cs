998338bace10d9c4bacd00877f37337af44be9bd
Sinan Çoban <sinan.coban@outlook.com>
2023-08-14 14:42:13 +0300

    Project Created

---
+++ Company.AdvertisementApp.Entities/Advertisement.cs
@@ -0,0 +1,10 @@
+﻿namespace Company.AdvertisementApp.Entities;
+
+public class Advertisement : BaseEntity
+{
+    public string? Title { get; set; }
+    public bool Status { get; set; }
+    public string? Description { get; set; }
+    public string? CreateDate { get; set; }
+    public List<AdvertisementAppUser>? AdvertisementAppUsers { get; set; }
+}
