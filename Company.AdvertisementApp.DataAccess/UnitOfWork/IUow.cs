04d58cb9d93bc0d6f71c56b73c952f38baaebd03
Sinan Çoban <sinan.coban@outlook.com>
2023-08-14 17:58:23 +0300

    UnitOfWork Added

---
+++ Company.AdvertisementApp.DataAccess/UnitOfWork/IUow.cs
@@ -0,0 +1,10 @@
+﻿using Company.AdvertisementApp.DataAccess.Interfaces;
+using Company.AdvertisementApp.Entities;
+
+namespace Company.AdvertisementApp.DataAccess.UnitOfWork;
+
+public interface IUow
+{
+    IRepository<T> GetRepository<T>() where T : BaseEntity;
+    Task SaveChangesAsync();
+}
---
+++ Company.AdvertisementApp.DataAccess/UnitOfWork/Uow.cs
@@ -0,0 +1,25 @@
+﻿using Company.AdvertisementApp.DataAccess.Contexts;
+using Company.AdvertisementApp.DataAccess.Interfaces;
+using Company.AdvertisementApp.DataAccess.Repositories;
+using Company.AdvertisementApp.Entities;
+
+namespace Company.AdvertisementApp.DataAccess.UnitOfWork;
+
+public class Uow
+{
+    private readonly AdvertisementContext _context;
+
+    public Uow(AdvertisementContext context)
+    {
+        _context = context;
+    }
+    
+    public IRepository<T> GetRepository<T>() where T : BaseEntity
+    {
+        return new Repository<T>(_context);
+    } 
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
