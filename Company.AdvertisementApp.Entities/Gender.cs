1a9e16750db2792d0db783bd70bd30fb2703f054
Sinan Çoban <sinan.coban@outlook.com>
2023-08-14 14:43:25 +0300

    Entities created

---
+++ Company.AdvertisementApp.Entities/Gender.cs
@@ -0,0 +1,7 @@
+﻿namespace Company.AdvertisementApp.Entities;
+
+public class Gender : BaseEntity
+{
+    public string? Definition { get; set; }
+    public List<AppUser>? AppUsers { get; set; }
+}
