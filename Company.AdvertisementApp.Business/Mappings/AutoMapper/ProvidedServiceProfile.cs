531e79abec672ee4801e364fc15523a3eabc6ced
Sinan Çoban <sinan.coban@outlook.com>
2023-08-17 08:42:30 +0300

    Added To Map Profile

---
+++ Company.AdvertisementApp.Business/Mappings/AutoMapper/ProvidedServiceProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Company.AdvertisementApp.Dto;
+using Company.AdvertisementApp.Entities;
+
+namespace Company.AdvertisementApp.Business.Mappings.AutoMapper;
+
+public class ProvidedServiceProfile : Profile
+{
+    public ProvidedServiceProfile()
+    {
+        CreateMap<ProvidedServiceCreateDto, ProvidedService>().ReverseMap();
+        CreateMap<ProvidedServiceUpdateDto, ProvidedService>().ReverseMap();
+        CreateMap<ProvidedServiceListDto, ProvidedService>().ReverseMap();
+    }
+}
