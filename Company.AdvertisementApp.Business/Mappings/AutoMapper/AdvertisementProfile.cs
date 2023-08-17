93c003cfb21b9368c3dc5ec265b5d047cd3e8fc9
Sinan Çoban <sinan.coban@outlook.com>
2023-08-17 17:05:20 +0300

    Map profiles added

---
+++ Company.AdvertisementApp.Business/Mappings/AutoMapper/AdvertisementProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Company.AdvertisementApp.Dto;
+using Company.AdvertisementApp.Entities;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Company.AdvertisementApp.Business.Mappings.AutoMapper;
+
+public class AdvertisementProfile : Profile
+{
+    public AdvertisementProfile()
+    {
+        CreateMap<Advertisement, AdvertisementListDto>().ReverseMap();
+        CreateMap<Advertisement, AdvertisementCreateDto>().ReverseMap();
+        CreateMap<Advertisement, AdvertisementUpdateDto>().ReverseMap();
+    }
+}
---
+++ Company.AdvertisementApp.Business/Mappings/AutoMapper/GenderProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Company.AdvertisementApp.Dto;
+using Company.AdvertisementApp.Entities;
+
+namespace Company.AdvertisementApp.Business.Mappings.AutoMapper;
+
+public class GenderProfile : Profile
+{
+    public GenderProfile()
+    {
+        CreateMap<Gender, GenderCreateDto>().ReverseMap();
+        CreateMap<Gender, GenderUpdateDto>().ReverseMap();
+        CreateMap<Gender, GenderListDto>().ReverseMap();
+    }
+}
