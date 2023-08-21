12c04eb32c0323b5dd03eb10bf9481c7eae151e7
Sinan Çoban <sinan.coban@outlook.com>
2023-08-21 17:36:43 +0300

    LoginDto and validator added

---
+++ Company.AdvertisementApp.Business/ValidationRules/AppUserLoginDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using Company.AdvertisementApp.Dto;
+using FluentValidation;
+
+namespace Company.AdvertisementApp.Business.ValidationRules;
+
+public class AppUserLoginDtoValidator : AbstractValidator<AppUserLoginDto>
+{
+    public AppUserLoginDtoValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty();
+        RuleFor(x => x.Password).NotEmpty();
+    }
+}
---
+++ Company.AdvertisementApp.Dto/AppUserDtos/AppUserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Company.AdvertisementApp.Dto;
+
+public class AppUserLoginDto
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public bool RememberMe { get; set; }
+}
