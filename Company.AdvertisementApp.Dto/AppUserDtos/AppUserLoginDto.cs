a23ce93434298eaff41dbbffb1008352ae1bf17f
Sinan Çoban <sinan.coban@outlook.com>
2023-08-21 21:50:21 +0300

    Fixed

--- Company.AdvertisementApp.Dto/AppUserDtos/AppUserLoginDto.cs
+++ Company.AdvertisementApp.Dto/AppUserDtos/AppUserLoginDto.cs
@@ -1,6 +1,8 @@
-﻿namespace Company.AdvertisementApp.Dto;
+﻿using Company.AdvertisementApp.Dto.Interfaces;
 
-public class AppUserLoginDto
+namespace Company.AdvertisementApp.Dto;
+
+public class AppUserLoginDto : IDto
 {
     public string UserName { get; set; }
     public string Password { get; set; }
