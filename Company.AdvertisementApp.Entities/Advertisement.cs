020314d3fc728bc97e924877b0a4aebf245b43fa
Sinan Çoban <sinan.coban@outlook.com>
2023-08-24 16:53:40 +0300

    fixed

--- Company.AdvertisementApp.Entities/Advertisement.cs
+++ Company.AdvertisementApp.Entities/Advertisement.cs
@@ -5,6 +5,6 @@ public class Advertisement : BaseEntity
     public string? Title { get; set; }
     public bool Status { get; set; }
     public string? Description { get; set; }
-    public string? CreateDate { get; set; }
+    public DateTime CreatedDate { get; set; }
     public List<AdvertisementAppUser>? AdvertisementAppUsers { get; set; }
 }
